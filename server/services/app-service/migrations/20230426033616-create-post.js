cf816c6ca087abac90b507d2b14668074b90073a
Rey <syahfauzi73@rocketmail.com>
2023-04-26 10:38:26 +0700

    added migration for Post, Category, and Tag

---
+++ server/services/app-service/migrations/20230426033616-create-post.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      slug: {
+        type: Sequelize.STRING
+      },
+      imgUrl: {
+        type: Sequelize.STRING
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER
+      },
+      AuthorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  }
+};
